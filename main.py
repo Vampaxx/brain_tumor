f59d0769dd7c304eab9ac52b9d035834bc3b6f69
Vampaxx <arjunappu1001@gmail.com>
2023-09-20 23:14:34 +0530

    modified virtual environment

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from src.brain_tumor.logger import logging
+from src.brain_tumor.exception import CustomException
+from src.brain_tumor.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logging.info(f">>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<")
+    obj     = DataIngestionTrainingPipeline()
+    obj.main()
+    logging.info((f">>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx======x"))
+except Exception as e:
+    logging.exception(e)
+    raise CustomException(e,sys) 
+
+    
+
+
